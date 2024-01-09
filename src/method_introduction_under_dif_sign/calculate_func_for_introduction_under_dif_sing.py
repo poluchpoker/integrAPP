b797cb748310a172046360cc048c17c35c231883
poluchpoker <90266390+poluchpoker@users.noreply.github.com>
2024-01-09 19:05:47 +0300

    Update calculate_func_for_introduction_under_dif_sing.py

--- src/method_introduction_under_dif_sign/calculate_func_for_introduction_under_dif_sing.py
+++ src/method_introduction_under_dif_sign/calculate_func_for_introduction_under_dif_sing.py
@@ -11,6 +11,15 @@ def trig_calc():
             res_element *= array_trig[element_tring]
 
         return res_element
+    
+def trig_calc1():
+    if len(array_trig1):
+        res_element = Symbol(f'{array_trig1[0]}')
+
+        for element_tring in range(1, len(array_trig1)):
+            res_element *= Symbol(f'{array_trig1[element_tring]}')
+
+        return res_element
 
 
 def result_all_func(
@@ -28,3 +37,19 @@ def result_all_func(
         return array_log[0] / result_polynom_for_argument_with_degree
     elif number_code == 10001:
         return array_pow[0] * result_polynom_for_argument_with_degree
+    
+def result_all_func1(
+    number_code, result_polynom_for_argument_with_degree
+):  # по кодовому номеру выполняем создание интеграла
+    if number_code == 111:
+        return trig_calc1()
+    elif number_code == 1101:
+        return trig_calc1() * result_polynom_for_argument_with_degree
+    elif number_code == 11:
+        return array_exp1[0] * result_polynom_for_argument_with_degree
+    elif number_code == 10011:
+        return array_log1[0] * result_polynom_for_argument_with_degree
+    elif number_code == 10010:
+        return array_log1[0] / result_polynom_for_argument_with_degree
+    elif number_code == 10001:
+        return array_pow1[0] * result_polynom_for_argument_with_degree
